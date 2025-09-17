eba6c8622d81f876fdc3c46db7dac3080cfd4d1b
Silver-PN <104447056+Silver-PN@users.noreply.github.com>
2025-09-17 08:42:34 +0700

    update code

--- _NhomTT__202509150144.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO public."NhomTT" ("NhomTTID","TenNhomTT","NgayCapNhat","NgayTao") VALUES
-	 ('AnHinhSuCN','Bản án/ quyết định hình sự',NULL,NULL),
-	 ('BanAnHinhSu','Bản án/ quyết định hình sự',NULL,NULL),
-	 ('TTKhieuKien','Thông tin khiếu kiện',NULL,NULL),
-	 ('QuyetDinhHonNhan','Quyết định ly hôn/hủy đăng ký kết hôn',NULL,NULL),
-	 ('BanAnDSHCKDTMLD','Bản án Dân sự/Hành chính/Kinh doanh thương mại/ Lao động',NULL,NULL),
-	 ('AnDSHCTMLDCN','Bản án Dân sự/Hành chính/Kinh doanh thương mại/ Lao động',NULL,NULL),
-	 ('NguoiVN','Thông tin cơ bản của công dân',NULL,NULL),
-	 ('CongDanNuocNgoai','Thông tin cơ bản của công dân',NULL,NULL);
---
+++ _TruongTT__202509170607.sql
@@ -0,0 +1,84 @@
+INSERT INTO public."TruongTT" ("ID","MoTa","PhuongThuc","TenTruongTT","TruongTTID","NgayCapNhat","NgayTao") VALUES
+	 ('T01','Mã bản án','Manual','Mã bản án','1',NULL,NULL),
+	 ('T02','Quan hệ pháp luật','Manual','Quan hệ pháp luật','2',NULL,NULL),
+	 ('T03','Họ và tên ','Manual','Họ và tên ','3',NULL,NULL),
+	 ('T04','Tên gọi khác','Manual','Tên gọi khác','4',NULL,NULL),
+	 ('T05','Số định danh cá nhân','Manual','Số định danh cá nhân','5',NULL,NULL),
+	 ('T06','Giới tính','Manual','Giới tính','6',NULL,NULL),
+	 ('T07','Ngày, tháng, năm sinh','Manual','Ngày, tháng, năm sinh','7',NULL,NULL),
+	 ('T08','Quốc tịch','Manual','Quốc tịch','8',NULL,NULL),
+	 ('T09','Nơi ở hiện tại - Thành phố/Tỉnh','Manual','Nơi ở hiện tại - Thành phố/Tỉnh','9',NULL,NULL),
+	 ('T010','Nơi ở hiện tại - Quận/Huyện','Manual','Nơi ở hiện tại - Quận/Huyện','10',NULL,NULL);
+INSERT INTO public."TruongTT" ("ID","MoTa","PhuongThuc","TenTruongTT","TruongTTID","NgayCapNhat","NgayTao") VALUES
+	 ('T011','Nơi ở hiện tại - Phường/Xã','Manual','Nơi ở hiện tại - Phường/Xã','11',NULL,NULL),
+	 ('T012','Nơi ở hiện tại - Địa chỉ','Manual','Nơi ở hiện tại - Địa chỉ','12',NULL,NULL),
+	 ('T013','Nơi đăng ký khai sinh - Thành phố/Tỉnh','Manual','Nơi đăng ký khai sinh - Thành phố/Tỉnh','13',NULL,NULL),
+	 ('T014','Nơi đăng ký khai sinh - Quận/Huyện','Manual','Nơi đăng ký khai sinh - Quận/Huyện','14',NULL,NULL),
+	 ('T015','Nơi đăng ký khai sinh - Phường/Xã','Manual','Nơi đăng ký khai sinh - Phường/Xã','15',NULL,NULL),
+	 ('T016','Nơi đăng ký khai sinh - Địa chỉ','Manual','Nơi đăng ký khai sinh - Địa chỉ','16',NULL,NULL),
+	 ('T017','Quê quán - Thành phố/Tỉnh','Manual','Quê quán - Thành phố/Tỉnh','17',NULL,NULL),
+	 ('T018','Quê quán - Quận/Huyện','Manual','Quê quán - Quận/Huyện','18',NULL,NULL),
+	 ('T019','Quê quán - Phường/Xã','Manual','Quê quán - Phường/Xã','19',NULL,NULL),
+	 ('T020','Quê quán - Địa chỉ','Manual','Quê quán - Địa chỉ','20',NULL,NULL);
+INSERT INTO public."TruongTT" ("ID","MoTa","PhuongThuc","TenTruongTT","TruongTTID","NgayCapNhat","NgayTao") VALUES
+	 ('T021','Nơi sinh - Thành phố/Tỉnh','Manual','Nơi sinh - Thành phố/Tỉnh','21',NULL,NULL),
+	 ('T022','Nơi sinh - Quận/Huyện','Manual','Nơi sinh - Quận/Huyện','22',NULL,NULL),
+	 ('T023','Nơi sinh - Phường/Xã','Manual','Nơi sinh - Phường/Xã','23',NULL,NULL),
+	 ('T024','Nơi sinh - Địa chỉ','Manual','Nơi sinh - Địa chỉ','24',NULL,NULL),
+	 ('T025','Nơi thường trú - Thành phố/Tỉnh','Manual','Nơi thường trú - Thành phố/Tỉnh','25',NULL,NULL),
+	 ('T026','Nơi thường trú - Quận/Huyện','Manual','Nơi thường trú - Quận/Huyện','26',NULL,NULL),
+	 ('T027','Nơi thường trú - Phường/Xã','Manual','Nơi thường trú - Phường/Xã','27',NULL,NULL),
+	 ('T028','Nơi thường trú - Địa chỉ','Manual','Nơi thường trú - Địa chỉ','28',NULL,NULL),
+	 ('T029','Nơi tạm trú - Thành phố/Tỉnh','Manual','Nơi tạm trú - Thành phố/Tỉnh','29',NULL,NULL),
+	 ('T030','Nơi tạm trú - Quận/Huyện','Manual','Nơi tạm trú - Quận/Huyện','30',NULL,NULL);
+INSERT INTO public."TruongTT" ("ID","MoTa","PhuongThuc","TenTruongTT","TruongTTID","NgayCapNhat","NgayTao") VALUES
+	 ('T031','Nơi tạm trú - Phường/Xã','Manual','Nơi tạm trú - Phường/Xã','31',NULL,NULL),
+	 ('T032','Nơi tạm trú - Địa chỉ','Manual','Nơi tạm trú - Địa chỉ','32',NULL,NULL),
+	 ('T033','Dân tộc','Manual','Dân tộc','33',NULL,NULL),
+	 ('T034','Tôn giáo','Manual','Tôn giáo','34',NULL,NULL),
+	 ('T035','Số định danh người nước ngoài hoặc Số giấy tờ của người nước ngoài được sử dụng nhập cảnh vào Việt Nam','Manual','Số định danh người nước ngoài hoặc Số giấy tờ của người nước ngoài được sử dụng nhập cảnh vào Việt Nam','35',NULL,NULL),
+	 ('T036','Tư cách tố tụng','Manual','Tư cách tố tụng','36',NULL,NULL),
+	 ('T037','Số bản án/quyết định','Manual','Số bản án/quyết định','37',NULL,NULL),
+	 ('T038','Ngày bản án/quyết định','Manual','Ngày bản án/quyết định','38',NULL,NULL),
+	 ('T039','Số bản án/ quyết định liên quan','Manual','Số bản án/ quyết định liên quan','39',NULL,NULL),
+	 ('T040','Ngày bản án/ quyết định liên quan','Manual','Ngày bản án/ quyết định liên quan','40',NULL,NULL);
+INSERT INTO public."TruongTT" ("ID","MoTa","PhuongThuc","TenTruongTT","TruongTTID","NgayCapNhat","NgayTao") VALUES
+	 ('T041','Nội dung bản án quyết đinh','Manual','Nội dung bản án quyết đinh','41',NULL,NULL),
+	 ('T042','Trạng thái quyết định bởi 1 quyết định khác','Manual','Trạng thái quyết định bởi 1 quyết định khác','42',NULL,NULL),
+	 ('T043','Bản án liên quan bị hủy bỏ/thay đổi','Manual','Bản án liên quan bị hủy bỏ/thay đổi','43',NULL,NULL),
+	 ('T044','Các quyết định bị hủy','Manual','Các quyết định bị hủy','44',NULL,NULL),
+	 ('T045','Trách nhiệm/Nghĩa vụ liên quan','Manual','Trách nhiệm/Nghĩa vụ liên quan','45',NULL,NULL),
+	 ('T046','Nghĩa vụ dân sự','Manual','Nghĩa vụ dân sự','46',NULL,NULL),
+	 ('T048','Cá nhân/Tổ chức là người Khởi kiện','Manual','Cá nhân/Tổ chức là người Khởi kiện','48',NULL,NULL),
+	 ('T049','Cá nhân/Tổ Chức là người bị kiện','Manual','Cá nhân/Tổ Chức là người bị kiện','49',NULL,NULL),
+	 ('T050','Cá nhân là người có quyền lợi, nghĩa vụ liên quan','Manual','Cá nhân là người có quyền lợi, nghĩa vụ liên quan','50',NULL,NULL),
+	 ('T051','Nội dung khởi kiện','Manual','Nội dung khởi kiện','51',NULL,NULL);
+INSERT INTO public."TruongTT" ("ID","MoTa","PhuongThuc","TenTruongTT","TruongTTID","NgayCapNhat","NgayTao") VALUES
+	 ('T052','Số bản án/ quyết định ','Manual','Số bản án/ quyết định ','52',NULL,NULL),
+	 ('T054','Loại giấy tờ','Manual','Loại giấy tờ','54',NULL,NULL),
+	 ('T055','Số bản án/quyết định ly hôn','Manual','Số bản án/quyết định ly hôn','55',NULL,NULL),
+	 ('T056','Ngày, tháng, năm ra bản án/ quyết định','Manual','Ngày, tháng, năm ra bản án/ quyết định','56',NULL,NULL),
+	 ('T057','Ngày hiệu lực của BA/QĐ ','Manual','Ngày hiệu lực của BA/QĐ ','57',NULL,NULL),
+	 ('T058','Trạng thái về tình trạng hôn nhân.1. Ly hôn 2. Hủy kết hôn trái pháp luật 3. Không công nhận là vợ chồng','Manual','Trạng thái về tình trạng hôn nhân. 1. Ly hôn 2. Hủy kết hôn trái pháp luật 3. Không công nhận là vợ chồng','58',NULL,NULL),
+	 ('T059','Số giấy chứng nhận kết hôn','Manual','Số giấy chứng nhận kết hôn','59',NULL,NULL),
+	 ('T060','Số Bản án/quyết định liên quan','Manual','Số Bản án/quyết định liên quan','60',NULL,NULL),
+	 ('T061','Tòa ra bản án/quyết định liên quan','Manual','Tòa ra bản án/quyết định liên quan','61',NULL,NULL),
+	 ('T062','Điều luật áp dụng','Manual','Điều luật áp dụng','62',NULL,NULL);
+INSERT INTO public."TruongTT" ("ID","MoTa","PhuongThuc","TenTruongTT","TruongTTID","NgayCapNhat","NgayTao") VALUES
+	 ('T063','Tội danh','Manual','Tội danh','63',NULL,NULL),
+	 ('T064','Hình phạt chính','Manual','Hình phạt chính','64',NULL,NULL),
+	 ('T065','Thời hạn/giá trị hình phạt chính','Manual','Thời hạn/giá trị hình phạt chính','65',NULL,NULL),
+	 ('T066','Hình phạt bổ sung','Manual','Hình phạt bổ sung','66',NULL,NULL),
+	 ('T067','Thời hạn/giá trị hình phạt bổ sung','Manual','Thời hạn/giá trị hình phạt bổ sung','67',NULL,NULL),
+	 ('T068','Thời hạn bị cấm','Manual','Thời hạn bị cấm','68',NULL,NULL),
+	 ('T047','Ngày bản án/quyết định liên quan','Manual','Ngày bản án/quyết định liên quan','47',NULL,NULL),
+	 ('T069','Thời điểm hiệu lực bản án','Manual','Thời điểm hiệu lực bản án','69',NULL,NULL),
+	 ('T070','Ngày Quyết định Thi hành án','Manual','Ngày Quyết định Thi hành án','70',NULL,NULL),
+	 ('T071','Số Quyết Định THA','Manual','Số Quyết Định THA','71',NULL,NULL);
+INSERT INTO public."TruongTT" ("ID","MoTa","PhuongThuc","TenTruongTT","TruongTTID","NgayCapNhat","NgayTao") VALUES
+	 ('T072','Tòa ra quyết định THA','Manual','Tòa ra quyết định THA','72',NULL,NULL),
+	 ('T073','Tình trạng thi hành án','Manual','Tình trạng thi hành án','73',NULL,NULL),
+	 ('T074','Ngày Thi hành bản án/quyết định','Manual','Ngày Thi hành bản án/quyết định','74',NULL,NULL),
+	 ('T075','Nơi thi hành bản án/quyết định','Manual','Nơi thi hành bản án/quyết định','75',NULL,NULL),
+	 ('T076','Lĩnh vực ','Manual','Lĩnh vực ','76',NULL,NULL),
+	 ('T053','Án phí','Manual','Án phí','53',NULL,NULL);
