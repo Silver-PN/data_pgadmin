dfbaac4bc543fc8afae94c1716630f3a5111d2b5
Silver-PN <104447056+Silver-PN@users.noreply.github.com>
2025-09-18 21:04:59 +0700

    f

--- _NhomTT__202509170607.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO public."NhomTT" ("NhomTTID","TenNhomTT","NgayCapNhat","NgayTao") VALUES
-	 ('1','Thông tin bản án',NULL,NULL),
-	 ('2','CMND, CCCD, Hộ chiếu, Hộ khẩu',NULL,NULL),
-	 ('3','Bản án/Quyết định',NULL,NULL),
-	 ('7','Mã bản án',NULL,NULL),
-	 ('4','Đơn khởi kiện',NULL,NULL),
-	 ('6','Quyết định thi hành án',NULL,NULL),
-	 ('5','Thông tin bản án/quyết định',NULL,NULL);
