404cc0b10cc0172dde7af42c06570e290b82575f
Silver-PN <104447056+Silver-PN@users.noreply.github.com>
2025-09-19 18:59:47 +0700

    update code git

---
+++ insert_truongtt.sql
@@ -0,0 +1,113 @@
+INSERT INTO public."TruongTT" ("MoTa","PhuongThuc","TenTruongTT","NgayCapNhat","NgayTao","MaDanhMuc") VALUES
+	 ('Mã bản án','Manual','Mã bản án','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Quan hệ pháp luật','Manual','Quan hệ pháp luật','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Họ và tên ','Manual','Họ và tên ','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Tên gọi khác','Manual','Tên gọi khác','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Số định danh cá nhân','Manual','Số định danh cá nhân','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Giới tính','Manual','Giới tính','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Ngày, tháng, năm sinh','Manual','Ngày, tháng, năm sinh','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Quốc tịch','Manual','Quốc tịch','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi ở hiện tại - Thành phố/Tỉnh','Manual','Nơi ở hiện tại - Thành phố/Tỉnh','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi ở hiện tại - Quận/Huyện','Manual','Nơi ở hiện tại - Quận/Huyện','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL);
+INSERT INTO public."TruongTT" ("MoTa","PhuongThuc","TenTruongTT","NgayCapNhat","NgayTao","MaDanhMuc") VALUES
+	 ('Nơi ở hiện tại - Phường/Xã','Manual','Nơi ở hiện tại - Phường/Xã','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi ở hiện tại - Địa chỉ','Manual','Nơi ở hiện tại - Địa chỉ','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi đăng ký khai sinh - Thành phố/Tỉnh','Manual','Nơi đăng ký khai sinh - Thành phố/Tỉnh','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi đăng ký khai sinh - Quận/Huyện','Manual','Nơi đăng ký khai sinh - Quận/Huyện','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi đăng ký khai sinh - Phường/Xã','Manual','Nơi đăng ký khai sinh - Phường/Xã','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi đăng ký khai sinh - Địa chỉ','Manual','Nơi đăng ký khai sinh - Địa chỉ','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Quê quán - Thành phố/Tỉnh','Manual','Quê quán - Thành phố/Tỉnh','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Quê quán - Quận/Huyện','Manual','Quê quán - Quận/Huyện','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Quê quán - Phường/Xã','Manual','Quê quán - Phường/Xã','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Quê quán - Địa chỉ','Manual','Quê quán - Địa chỉ','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL);
+INSERT INTO public."TruongTT" ("MoTa","PhuongThuc","TenTruongTT","NgayCapNhat","NgayTao","MaDanhMuc") VALUES
+	 ('Nơi sinh - Thành phố/Tỉnh','Manual','Nơi sinh - Thành phố/Tỉnh','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi sinh - Quận/Huyện','Manual','Nơi sinh - Quận/Huyện','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi sinh - Phường/Xã','Manual','Nơi sinh - Phường/Xã','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi sinh - Địa chỉ','Manual','Nơi sinh - Địa chỉ','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi thường trú - Thành phố/Tỉnh','Manual','Nơi thường trú - Thành phố/Tỉnh','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi thường trú - Quận/Huyện','Manual','Nơi thường trú - Quận/Huyện','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi thường trú - Phường/Xã','Manual','Nơi thường trú - Phường/Xã','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi thường trú - Địa chỉ','Manual','Nơi thường trú - Địa chỉ','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi tạm trú - Thành phố/Tỉnh','Manual','Nơi tạm trú - Thành phố/Tỉnh','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi tạm trú - Quận/Huyện','Manual','Nơi tạm trú - Quận/Huyện','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL);
+INSERT INTO public."TruongTT" ("MoTa","PhuongThuc","TenTruongTT","NgayCapNhat","NgayTao","MaDanhMuc") VALUES
+	 ('Nơi tạm trú - Phường/Xã','Manual','Nơi tạm trú - Phường/Xã','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi tạm trú - Địa chỉ','Manual','Nơi tạm trú - Địa chỉ','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Dân tộc','Manual','Dân tộc','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Tôn giáo','Manual','Tôn giáo','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Số định danh người nước ngoài hoặc Số giấy tờ của người nước ngoài được sử dụng nhập cảnh vào Việt Nam','Manual','Số định danh người nước ngoài hoặc Số giấy tờ của người nước ngoài được sử dụng nhập cảnh vào Việt Nam','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Tư cách tố tụng','Manual','Tư cách tố tụng','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Số bản án/quyết định','Manual','Số bản án/quyết định','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Ngày bản án/quyết định','Manual','Ngày bản án/quyết định','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Số bản án/ quyết định liên quan','Manual','Số bản án/ quyết định liên quan','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Ngày bản án/ quyết định liên quan','Manual','Ngày bản án/ quyết định liên quan','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL);
+INSERT INTO public."TruongTT" ("MoTa","PhuongThuc","TenTruongTT","NgayCapNhat","NgayTao","MaDanhMuc") VALUES
+	 ('Nội dung bản án quyết đinh','Manual','Nội dung bản án quyết đinh','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Trạng thái quyết định bởi 1 quyết định khác','Manual','Trạng thái quyết định bởi 1 quyết định khác','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Bản án liên quan bị hủy bỏ/thay đổi','Manual','Bản án liên quan bị hủy bỏ/thay đổi','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Các quyết định bị hủy','Manual','Các quyết định bị hủy','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Trách nhiệm/Nghĩa vụ liên quan','Manual','Trách nhiệm/Nghĩa vụ liên quan','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nghĩa vụ dân sự','Manual','Nghĩa vụ dân sự','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Án phí','Manual','Án phí','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Cá nhân/Tổ chức là người Khởi kiện','Manual','Cá nhân/Tổ chức là người Khởi kiện','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Cá nhân/Tổ Chức là người bị kiện','Manual','Cá nhân/Tổ Chức là người bị kiện','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Cá nhân là người có quyền lợi, nghĩa vụ liên quan','Manual','Cá nhân là người có quyền lợi, nghĩa vụ liên quan','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL);
+INSERT INTO public."TruongTT" ("MoTa","PhuongThuc","TenTruongTT","NgayCapNhat","NgayTao","MaDanhMuc") VALUES
+	 ('Nội dung khởi kiện','Manual','Nội dung khởi kiện','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Số bản án/ quyết định ','Manual','Số bản án/ quyết định ','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Ngày bản án/quyết định liên quan','Manual','Ngày bản án/quyết định liên quan','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Loại giấy tờ','Manual','Loại giấy tờ','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Số bản án/quyết định ly hôn','Manual','Số bản án/quyết định ly hôn','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Ngày, tháng, năm ra bản án/ quyết định','Manual','Ngày, tháng, năm ra bản án/ quyết định','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Ngày hiệu lực của BA/QĐ ','Manual','Ngày hiệu lực của BA/QĐ ','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Trạng thái về tình trạng hôn nhân.1. Ly hôn 2. Hủy kết hôn trái pháp luật3. Không công nhận là vợ chồng','Manual','Trạng thái về tình trạng hôn nhân.1. Ly hôn2. Hủy kết hôn trái pháp luật3. Không công nhận là vợ chồng','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Số giấy chứng nhận kết hôn','Manual','Số giấy chứng nhận kết hôn','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Số Bản án/quyết định liên quan','Manual','Số Bản án/quyết định liên quan','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL);
+INSERT INTO public."TruongTT" ("MoTa","PhuongThuc","TenTruongTT","NgayCapNhat","NgayTao","MaDanhMuc") VALUES
+	 ('Tòa ra bản án/quyết định liên quan','Manual','Tòa ra bản án/quyết định liên quan','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Điều luật áp dụng','Manual','Điều luật áp dụng','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Tội danh','Manual','Tội danh','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Hình phạt chính','Manual','Hình phạt chính','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Thời hạn/giá trị hình phạt chính','Manual','Thời hạn/giá trị hình phạt chính','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Hình phạt bổ sung','Manual','Hình phạt bổ sung','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Thời hạn/giá trị hình phạt bổ sung','Manual','Thời hạn/giá trị hình phạt bổ sung','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Thời hạn bị cấm','Manual','Thời hạn bị cấm','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Thời điểm hiệu lực bản án','Manual','Thời điểm hiệu lực bản án','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Ngày Quyết định Thi hành án','Manual','Ngày Quyết định Thi hành án','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL);
+INSERT INTO public."TruongTT" ("MoTa","PhuongThuc","TenTruongTT","NgayCapNhat","NgayTao","MaDanhMuc") VALUES
+	 ('Số Quyết Định THA','Manual','Số Quyết Định THA','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Tòa ra quyết định THA','Manual','Tòa ra quyết định THA','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Tình trạng thi hành án','Manual','Tình trạng thi hành án','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Ngày Thi hành bản án/quyết định','Manual','Ngày Thi hành bản án/quyết định','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi thi hành bản án/quyết định','Manual','Nơi thi hành bản án/quyết định','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Lĩnh vực ','Manual','Lĩnh vực ','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Ngày, tháng, năm','Manual','Ngày, tháng, năm','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Bản án/Quyết định liên quan - Số Bản án/Quyết định','Manual','Bản án/Quyết định liên quan - Số Bản án/Quyết định','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Bản án/Quyết định liên quan - Ngày, tháng, năm','Manual','Bản án/Quyết định liên quan - Ngày, tháng, năm','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Bản án/Quyết định liên quan- Toà án ban hành','Manual','Bản án/Quyết định liên quan- Toà án ban hành','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL);
+INSERT INTO public."TruongTT" ("MoTa","PhuongThuc","TenTruongTT","NgayCapNhat","NgayTao","MaDanhMuc") VALUES
+	 ('Nơi thường trú (xem trong CCCD hoặc địa chỉ ghi trong Bản án/Quyết định) - Thành phố/Tỉnh','Manual','Nơi thường trú (xem trong CCCD hoặc địa chỉ ghi trong Bản án/Quyết định) - Thành phố/Tỉnh','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi thường trú (xem trong CCCD hoặc địa chỉ ghi trong Bản án/Quyết định) - Quận/Huyện','Manual','Nơi thường trú (xem trong CCCD hoặc địa chỉ ghi trong Bản án/Quyết định) - Quận/Huyện','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi thường trú (xem trong CCCD hoặc địa chỉ ghi trong Bản án/Quyết định) - Phường/Xã','Manual','Nơi thường trú (xem trong CCCD hoặc địa chỉ ghi trong Bản án/Quyết định) - Phường/Xã','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi thường trú (xem trong CCCD hoặc địa chỉ ghi trong Bản án/Quyết định) - Địa chỉ','Manual','Nơi thường trú (xem trong CCCD hoặc địa chỉ ghi trong Bản án/Quyết định) - Địa chỉ','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Tên cơ quan, tổ chức','Manual','Tên cơ quan, tổ chức','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Số đăng ký kinh doanh','Manual','Số đăng ký kinh doanh','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Mã số thuế','Manual','Mã số thuế','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Địa chỉ trụ sở cơ quan, tổ chức - Thành phố/Tỉnh','Manual','Địa chỉ trụ sở cơ quan, tổ chức - Thành phố/Tỉnh','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Địa chỉ trụ sở cơ quan, tổ chức - Quận/Huyện','Manual','Địa chỉ trụ sở cơ quan, tổ chức - Quận/Huyện','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Địa chỉ trụ sở cơ quan, tổ chức - Phường/Xã','Manual','Địa chỉ trụ sở cơ quan, tổ chức - Phường/Xã','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL);
+INSERT INTO public."TruongTT" ("MoTa","PhuongThuc","TenTruongTT","NgayCapNhat","NgayTao","MaDanhMuc") VALUES
+	 ('Địa chỉ trụ sở cơ quan, tổ chức - Địa chỉ','Manual','Địa chỉ trụ sở cơ quan, tổ chức - Địa chỉ','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Bản án/Quyết định liên quan - Số Bản án/ Quyết định ','Manual','Bản án/Quyết định liên quan - Số Bản án/ Quyết định ','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Bản án/Quyết định liên quan - Toà án ban hành','Manual','Bản án/Quyết định liên quan - Toà án ban hành','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi thường trú (xem trong CCCD hoặc địa chỉ ghi trong Bản án/Quyết định)- Thành phố/Tỉnh','Manual','Nơi thường trú (xem trong CCCD hoặc địa chỉ ghi trong Bản án/Quyết định)- Thành phố/Tỉnh','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi thường trú (xem trong CCCD hoặc địa chỉ ghi trong Bản án/Quyết định)- Quận/Huyện','Manual','Nơi thường trú (xem trong CCCD hoặc địa chỉ ghi trong Bản án/Quyết định)- Quận/Huyện','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi thường trú (xem trong CCCD hoặc địa chỉ ghi trong Bản án/Quyết định)- Phường/Xã','Manual','Nơi thường trú (xem trong CCCD hoặc địa chỉ ghi trong Bản án/Quyết định)- Phường/Xã','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Nơi thường trú (xem trong CCCD hoặc địa chỉ ghi trong Bản án/Quyết định)- Địa chỉ','Manual','Nơi thường trú (xem trong CCCD hoặc địa chỉ ghi trong Bản án/Quyết định)- Địa chỉ','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Trạng thái về tình trạng hôn nhân','Manual','Trạng thái về tình trạng hôn nhân','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Số Quyết định thi hành án','Manual','Số Quyết định thi hành án','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Ngày, tháng, năm của Bản án/Quyết định','Manual','Ngày, tháng, năm của Bản án/Quyết định','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL);
+INSERT INTO public."TruongTT" ("MoTa","PhuongThuc","TenTruongTT","NgayCapNhat","NgayTao","MaDanhMuc") VALUES
+	 ('Tòa án ban hành Bản án/Quyết định','Manual','Tòa án ban hành Bản án/Quyết định','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL),
+	 ('Ngày thi hành','Manual','Ngày thi hành','2025-09-19 05:34:28.80604','2025-09-19 05:34:28.80604',NULL);
